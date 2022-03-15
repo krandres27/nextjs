a64e4b6456ad22e999860a3ce5a2857b7a98b477
Carlos Andres Gonzalez <cgonzalez@fullstacklabs.co>
2022-03-15 16:46:52 -0500

    event detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,23 @@
+import { useRouter } from 'next/router'
+import { getEventById } from '../../feed'
+import EventContent from '../../components/event-detail/eventContent'
+import EventSummary from '../../components/event-detail/EventSummary'
+import EventLogistics from '../../components/event-detail/EventLogistics'
+
 const EventDetail = () => {
-  return <p>EventDetail view</p>
+  const router = useRouter();
+  const { query: { eventId }} = router
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return (<p>No Event found!</p>)
+  }
+
+  return <>
+    <EventSummary title={event.title} />
+    <EventLogistics event={event} />
+    <EventContent>{event.description}</EventContent>
+  </>
 }
 
 export default EventDetail
